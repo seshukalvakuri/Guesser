89bc53db6657e558afd760ded4fa5fe418aa017f
Student <student@nw45510.nwmissouri.edu>
2019-02-26 18:51:02 -0600

    Initial Commit

---
+++ Guesser/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Guesser
+//
+//  Created by Student on 2/26/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Guesser/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Guesser
+//
+//  Created by Student on 2/26/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
